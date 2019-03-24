d86db7f06bcaf8102a952785773988048aca6f64
Billy Murray <wmurray8989@gmail.com>
2019-03-23 23:39:15 -0400

    Fixed bug with DirectionTo

--- position/directionTo.go
+++ position/directionTo.go
@@ -8,23 +8,23 @@ func (p Position) DirectionTo(other Position) direction.Direction {
 	xDistance := other.X - p.X
 	yDistance := other.Y - p.Y
 
-	if (xDistance == 0 && yDistance == 0) {
+	if xDistance == 0 && yDistance == 0 {
 		return direction.Null
 	}
 
-	if (xDistance > yDistance) {
+	if integerAbs(xDistance) > integerAbs(yDistance) {
 		// horizontal
-		if (xDistance > 0) {
+		if xDistance > 0 {
 			return direction.Right
 		} else {
-			return direction.Left 
+			return direction.Left
 		}
-	} else{
+	} else {
 		// vertical
-		if (yDistance > 0) {
+		if yDistance > 0 {
 			return direction.Up
 		} else {
-			return direction.Down 
+			return direction.Down
 		}
 	}
 }
