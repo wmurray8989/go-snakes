d94f153c26dd6b03c608c880bb41951ef83a8042
Billy Murray <wmurray8989@gmail.com>
2019-03-24 00:00:25 -0400

    Added comments for directions

--- direction/direction.go
+++ direction/direction.go
@@ -3,8 +3,17 @@ package direction
 // Direction contains an X and Y coordinate
 type Direction int
 
+// Null is the lack of direction
 const Null Direction = -1
+
+// Up is the direction towards the top of the grid
 const Up Direction = 0
+
+// Right is the direction towards the right of the grid
 const Right Direction = 1
+
+// Down is the direction towards the bottom of the grid
 const Down Direction = 2
+
+// Left is the direction towards the left of the grid
 const Left Direction = 3
