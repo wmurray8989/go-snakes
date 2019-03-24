a76ce14413b07879ee5eb3c8e804925706097e2a
Billy Murray <wmurray8989@gmail.com>
2019-03-24 10:56:01 -0400

    Fixed crash on second round

--- match/newMatch.go
+++ match/newMatch.go
@@ -13,6 +13,10 @@ func NewMatch(player1 round.Strategy, player2 round.Strategy) Match {
 	// record start time
 	match.startTime = time.Now().UTC()
 
+	// Players
+	match.player1 = player1
+	match.player2 = player2
+
 	// create first round
 	match.activeRound = round.NewRound(player1, player2)
 
