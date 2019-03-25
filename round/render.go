989cff1f5f1ece310222d2876b7e60c8832340f7
Billy Murray <wmurray8989@gmail.com>
2019-03-24 21:23:15 -0400

    Disabled grid drawing

--- round/render.go
+++ round/render.go
@@ -1,7 +1,6 @@
 package round
 
 import (
-	"github.com/veandco/go-sdl2/gfx"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
@@ -15,16 +14,16 @@ func (p *Round) Render(renderer *sdl.Renderer) {
 	p.player2.Render(renderer)
 
 	// draw grid
-	for x := int32(0); x < sideLength*cellSize; x = x + cellSize {
-		for y := int32(0); y < sideLength*cellSize; y = y + cellSize {
-			gfx.RectangleColor(
-				renderer,
-				int32(x),
-				int32(y),
-				int32(x+cellSize),
-				int32(y+cellSize),
-				p.gridColor,
-			)
-		}
-	}
+	// for x := int32(0); x < sideLength*cellSize; x = x + cellSize {
+	// 	for y := int32(0); y < sideLength*cellSize; y = y + cellSize {
+	// 		gfx.RectangleColor(
+	// 			renderer,
+	// 			int32(x),
+	// 			int32(y),
+	// 			int32(x+cellSize),
+	// 			int32(y+cellSize),
+	// 			p.gridColor,
+	// 		)
+	// 	}
+	// }
 }
