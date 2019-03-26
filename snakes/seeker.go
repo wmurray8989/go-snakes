991c91c87daa0d95c0b8debbd831875f13d28156
Billy Murray <wmurray8989@gmail.com>
2019-03-25 20:21:56 -0400

    Modified seeker color

--- snakes/seeker.go
+++ snakes/seeker.go
@@ -12,8 +12,8 @@ import (
 var Seeker = player.Player{
 	Name:           "Seeker",
 	Author:         "Null",
-	ColorPrimary:   sdl.Color{R: 255, G: 255, B: 255, A: 255},
-	ColorSecondary: sdl.Color{R: 0, G: 0, B: 0, A: 255},
+	ColorPrimary:   sdl.Color{R: 0, G: 255, B: 0, A: 255},
+	ColorSecondary: sdl.Color{R: 255, G: 0, B: 255, A: 255},
 	Strategy: func(self []position.Position, opponent []position.Position) position.Position {
 		currentPosition := self[len(self)-1]
 		opponentPosition := opponent[len(opponent)-1]
